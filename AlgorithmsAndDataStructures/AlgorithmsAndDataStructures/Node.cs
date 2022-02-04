e8821499c120d7c6117d89f184cbcf8108237a37
DmitriyTislyuk <tislyukdmitriy@mail.ru>
2022-02-04 18:40:46 +0300

    Добавлено домашее задание №2

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+    }
+}
